65c8bc909b3cfc5cfc0d488e0f742fdacdc89fdd
Vito <vitodro@gmail.com>
2020-07-04 17:43:19 +0200

    več popravkov v programu, dopolnjen README, dodana komanda za pomoč

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Študentska prehrana
 ### Discord bot
 
-**TODO: Search and selection of providers**
+## Setup
+Before usage, a new Discord application has to be created, as shown in the [instructions](http://discordpy.readthedocs.io/en/latest/discord.html), where the private key is aquired. The key is used when running the application for authentication. It has to be placed into a file next to the bot file, or directly in the client.run command.
 
 ## Usage
-Before usage, a new Discord application has to be created, as shown in the [instructions](http://discordpy.readthedocs.io/en/latest/discord.html), where the private key is aquired. The key is used when running the application for authentication. It has to be placed into a file next to the bot file, or directly in the client.run command.
+The bot has 3 commands:
+* !pomoc
+    usage: `!pomoc`
+    the bot sends the help message
+* !hrana
+    usage: `!hrana`
+    the bot sends the menu of the chosen restaurant. Which restaurant has been chosen in this instance can be seen in the help message or in the bot.py file where the value is assigned to a variable called "default"
+* !ponudba
+    usage: `!ponudba <query>`
+    the bot sends the menu of the restaurant that has the entered query in it's name. If there are more restaurants like that, bot sends a numbered list with all those restaurants and waits for the user to reply with a number that is in front of the restaurant he was looking for. If there are multiple restaurants with queryed text in their names, anwser has to be chosen from the list in one minute!
--- bot.py
+++ bot.py
@@ -13,23 +13,32 @@ client = commands.Bot(command_prefix="!")
 async def on_ready():
     print("Bot is ready")
 
+@client.command(name="pomoc")
+async def pomoc(ctx):
+    sporocilo="""
+    poleg ukaza pomoc, bot vsebuje tudi ukaza:
+    -hrana:
+        uporaba: `!hrana`
+        Izpiše ponudbo izbrane restavracije (to izbere uprabitelj bota).
+        Ta različica izpiše ponudbo restavracije `{}`
+    -ponudba:
+        uporaba `!ponudba <iskalni_niz>`
+        Izpiše ponudbo iskane restavracije. Če je iskani niz v imenih večih restavracij, bot ponudi seznam za izbor.
+        Uporabnik izbira z odgovorom v obliki ustrezne številke.
+    """.format(default_name)
+    await ctx.send(sporocilo)
+
 # ob prejetju ukaza hrana ("!hrana") izpiše vso ponudbo restavracije, ki je določena v programu
 # when command hrana ("!hrana") is called, entire menu of the chosen restaurant is sent
 @client.command(name="hrana")
 async def hrana(ctx):
     await ctx.send("**"+restaurant+"**")
-    if len(food_list)>0:
-        food_message = "```\n"
-        food_message += "\n".join(food_list)
-        food_message += "```"
-    else:
-        food_message = "Ponudnik danes nima ponudbe."
-    await ctx.send(food_message)
+    await ctx.send(get_menu_message(default))
     now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    current_time = now.strftime("%D %H:%M:%S")
     # izpis v konzolo za boljši nadzor
     # output to the console for better control
-    print("Request for "+restaurant+"menu complete: ", current_time)
+    print("Request for "+restaurant+" menu complete: ", current_time)
 
 # ob prejetju ukaza ponudba ("!ponudba <poizvedba>") izpiše ponudbo iskane restavracije, če se s poivedbo ujema
 #   več ponudnikov, uporabnik izbere iskanega
@@ -38,7 +47,7 @@ async def hrana(ctx):
 @client.command(name="ponudba")
 async def ponudba(ctx, *, query):
     now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    current_time = now.strftime("%D %H:%M:%S")
     found = False
     options = []
     for key in providers:
@@ -63,13 +72,13 @@ async def ponudba(ctx, *, query):
             return
 
         def check(m):
-            return int(m.content) >= 1 and int(m.content) <= len(options)
+            return int(m.content) >= 1 and int(m.content) <= len(options) and ctx.channel.id == m.channel.id
 
-        msg = await client.wait_for('message', check=check)
+        msg = await client.wait_for('message', check=check, timeout=60)
         await ctx.send("**"+options[int("{.content}".format(msg))-1]+"**")
         await ctx.send(get_menu_message(providers[options[int("{.content}".format(msg))-1]]))
         found = True
-        print("Request for", options[0], "menu complete: ", current_time)
+        print("Request for", options[int("{.content}".format(msg))-1], "menu complete: ", current_time)
     if not found:
         await ctx.send("**Med ponudniki ni zadetka, poskusite ponovno.**")
         print("Provider", query, "not found!", current_time)
@@ -80,7 +89,8 @@ from bs4 import BeautifulSoup
 
 # tu se lahko izbere katerakoli restavracija
 # here any of the providers can be chosen
-url = "https://www.studentska-prehrana.si/restaurant/Details/2529"
+default=2529
+url = "https://www.studentska-prehrana.si/restaurant/Details/"+str(default)
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
 page = requests.get(url, headers=headers)
@@ -88,7 +98,8 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 cards = [str(card) for card in soup.find_all(class_="text-bold color-blue")]
 restaurant = [str(niz) for niz in soup.find_all(class_="no-margin bold")][0].replace('<h3 class="no-margin bold">', "").replace('</h3>', "").strip()
-food_list = [card.replace('<p class="text-bold color-blue"><h5><strong class="color-blue">', "").replace('</strong></h5></p>', "") for card in cards]
+
+default_name=restaurant
 
 # DOBIVANJE IMENIKA PONUDNIKOV
 def get_providers():
