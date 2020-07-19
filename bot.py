6206ef3399499040661ceb9030f99589b894b6a7
Vito <vitodro@gmail.com>
2020-07-19 13:06:50 +0200

    dodano poizvedovanje o informacijah ponudnika

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 ### DISCORD DEL
 from datetime import datetime
+from datetime import timedelta
 from discord.ext import commands
 
 # tukaj se lahko spremeni predpona, za katero bot posluša
@@ -15,14 +16,18 @@ async def on_ready():
 @client.command(name="pomoc")
 async def pomoc(ctx):
     sporocilo="""
-    poleg ukaza pomoc, bot vsebuje tudi ukaza:
+    poleg ukaza pomoc, bot vsebuje tudi ukaze:
     -hrana:
         uporaba: `!hrana`
         Izpiše ponudbo izbrane restavracije (to izbere uprabitelj bota).
         Ta različica izpiše ponudbo restavracije `{}`
     -ponudba:
-        uporaba `!ponudba <iskalni_niz>`
+        uporaba: `!ponudba <iskalni_niz>`
         Izpiše ponudbo iskane restavracije. Če je iskani niz v imenih večih restavracij, bot ponudi seznam za izbor.
+        Uporabnik izbira z odgovorom v obliki ustrezne številke.
+    -informacije:
+        uporaba: `!informacije <iskalni_niz>`
+        Izpiše informacije o iskani restavraiciji. Če je iskalni niz v imenih večij restavracij, bot ponudi seznam za izbor.
         Uporabnik izbira z odgovorom v obliki ustrezne številke.
     """.format(default_name)
     await ctx.send(sporocilo)
@@ -78,6 +83,48 @@ async def ponudba(ctx, *, query):
         await ctx.send(get_menu_message(providers[options[int("{.content}".format(msg))-1]]))
         found = True
         print("[general] Request for", options[int("{.content}".format(msg))-1], "menu complete: ", current_time)
+    if not found:
+        await ctx.send("**Med ponudniki ni zadetka, poskusite ponovno.**")
+        print("[general] Provider", query, "not found: "+current_time)
+
+
+# ob prejetju ukaza ponudba ("!informacije <poizvedba>") izpiše informacije o iskani restavraciji, če se s poivedbo ujema
+#   več ponudnikov, uporabnik izbere iskanega
+# when command ("!informacije <query>") informations about the searched restaurant are sent, if query matches multiple providers,
+#   user chooses the one he is looking for
+@client.command(name="informacije")
+async def ponudba(ctx, *, query):
+    now = datetime.now()
+    current_time = now.strftime("%D %H:%M:%S")
+    found = False
+    options = []
+    for key in providers:
+        if query.lower() in key.lower():
+            options.append(key)
+    if len(options) == 1:
+        await ctx.send("**"+options[0]+"**\n"+get_info_message(providers[options[0]]))
+        found = True
+        print("[general] Request for", options[0], "informations complete: ", current_time)
+    elif len(options) > 1:
+        list = "```"
+        for index, option in enumerate(options):
+            list += ('{:<4}'.format(str(index+1))+"  "+option+"\n")
+        list += "```"
+        if len(list) <= 2000:
+            await ctx.send("**Več ponudnikov vsebuje iskan niz.**\nOdgovorite z številko, ki je na seznamu pred ponudnikom, katerega informacije želite.")
+            await ctx.send(list)
+        else:
+            await ctx.send("**Iskani niz je preveč splošen, poskusite ponovno.**")
+            print("[general] Search to broad for query", query+": "+current_time)
+            return
+
+        def check(m):
+            return 1 <= int(m.content) <= len(options) and ctx.channel.id == m.channel.id
+
+        msg = await client.wait_for('message', check=check, timeout=60)
+        await ctx.send("**"+options[int("{.content}".format(msg))-1]+"**\n"+get_info_message(providers[options[int("{.content}".format(msg))-1]]))
+        found = True
+        print("[general] Request for", options[int("{.content}".format(msg))-1], "informations: ", current_time)
     if not found:
         await ctx.send("**Med ponudniki ni zadetka, poskusite ponovno.**")
         print("[general] Provider", query, "not found: "+current_time)
@@ -155,6 +202,32 @@ def scrape_menu(ID):
         return food_message
     else:
         return food_message[:1966] + "\nPrikazan je samo delni meni```"
+
+
+# DOBIVANJE INFORMACIJ
+def get_info_message(ID):
+    url = "https://www.studentska-prehrana.si/restaurant/Details/" + str(ID)
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    informacije = soup.find(class_="col-md-6")
+    naslov = str(informacije.find("small")).replace("<small>", "").replace("</small>", "")
+    if len(naslov) == 0:
+        naslov = "Ponudnik nima vpisanega naslova"
+    telefonska = naslov.split("(")[1].replace(")", "")
+    if len(telefonska) == 0:
+        telefonska = "Ponudnik nima vpisane telefonske številke"
+    info_message = "Naslov: "
+    info_message += naslov.split("(")[0] + "\nTelefonska številka: " + telefonska
+    doplacilo = str(informacije.find(class_="color-light-grey")).replace('<span class="color-light-grey">', "").replace("</span>", "")
+    if len(doplacilo) == 0:
+        doplacilo = "Ponudnik nima vpisanega zneska doplačila"
+    info_message += "\nDoplačilo: " + doplacilo
+    casi = str(soup.find_all(class_="col-md-12 text-bold"))
+    med_tednom = casi.split("<br/>")[1].strip()
+    sobota = casi.split("<br/>")[4].strip()
+    nedelja = casi.split("<br/>")[7].replace("</div>]", "").strip()
+    info_message += "\nOdpiralni časi:\n\tMed tednom: "+med_tednom+"\n\tSobota: "+sobota+"\n\tNedelja in prazniki: "+nedelja
+    return info_message
 
 
 # ERROR HANDLING
